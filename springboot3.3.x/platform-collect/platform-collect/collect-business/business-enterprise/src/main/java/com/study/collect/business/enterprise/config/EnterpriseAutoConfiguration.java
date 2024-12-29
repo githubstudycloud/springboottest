77a9f7c15fa66b1362566f8947fb058f50c19e32
shindou <664711649@qq.com>
2024-12-30 06:52:44 +0800

    init

--- springboot3.3.x/platform-collect/platform-collect/collect-business/business-enterprise/src/main/java/com/study/collect/business/enterprise/config/EnterpriseAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.study.collect.business.enterprise.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.study.collect.business.enterprise")
-public class EnterpriseAutoConfiguration {
-}
